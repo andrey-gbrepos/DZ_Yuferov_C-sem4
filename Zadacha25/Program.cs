d38474980ec116740f6aa2679cded9b4675a2a14
andrey-gbrepose <yandire@yandex.ru>
2023-07-03 20:26:35 +0300

    Add Zadacha15

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+//возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Clear(); 
+Console.WriteLine("Программа принимает на вход два числа A и B и возводит число A в натуральную степень B");
+Console.WriteLine();
+int[] arrAB = new int[2];
+FilArr (arrAB);
+Console.WriteLine($"{arrAB[0]} ^ {arrAB[1]} = {Math.Pow(arrAB[0], arrAB[1])}");
+Console.WriteLine();
+
+int[] FilArr (int[] arr)
+{
+Console.Write("Введите число A: ");
+arr[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите число B: ");
+arr[1] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();  
+return arr;
+}
+
+
+
+
+
