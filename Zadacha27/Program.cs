8604d6ddda6d1fa3b28d8c107aefa4f8e5c8efce
andrey-gbrepose <yandire@yandex.ru>
2023-07-03 21:34:21 +0300

    Add Zadacha27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт 
+//сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Clear(); 
+Console.WriteLine("Программа принимает на вход число и выдаёт сумму цифр в числе.");
+Console.WriteLine();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр --> {SumNumber(number)}");
+Console.WriteLine(" ");
+
+int SumNumber (int num)
+{
+    if (num < 0) num *= -1;
+    int sum = 0;
+    for (int i = 0; num > 0; i++)  
+        { 
+            sum = sum + num%10;
+            num /= 10;  
+        }  
+    return sum;
+}
