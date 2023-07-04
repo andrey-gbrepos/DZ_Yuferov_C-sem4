5f5d8e86c5b06f65a628ee06a93eded8eec4e231
andrey-gbrepose <yandire@yandex.ru>
2023-07-04 13:40:14 +0300

    Add Zadacha29 & Change Zad25

--- Zadacha25/Program.cs
+++ Zadacha25/Program.cs
@@ -5,6 +5,36 @@
 
 Console.Clear(); 
 Console.WriteLine("Программа принимает на вход два числа A и B и возводит число A в натуральную степень B");
+                                                Console.WriteLine();
+Console.Write("Введите число A: ");
+double A = Convert.ToDouble(Console.ReadLine());
+                                                Console.WriteLine();
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+                                                Console.WriteLine();  
+if (B < 0)
+{
+    Console.WriteLine("Степень должна быть целым положительным числом");
+    return;
+}
+
+Console.Write($"{A} ^ {B} = {Math.Round(MyPow(A,B),2)}"); 
+
+double MyPow (double num, int pow)
+{
+    double temp = 1;
+  for (int i = 1; i<pow; i++)  
+    {    
+         temp = temp*num;
+    }
+return temp;
+}
+
+
+
+
+/* Console.Clear(); 
+Console.WriteLine("Программа принимает на вход два числа A и B и возводит число A в натуральную степень B");
 Console.WriteLine();
 int[] arrAB = new int[2];
 FilArr (arrAB);
@@ -20,7 +50,7 @@ Console.Write("Введите число B: ");
 arr[1] = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();  
 return arr;
-}
+} */
 
 
 
---
+++ Zadacha29/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и 
+//выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+Console.Clear(); 
+Console.WriteLine("Программа задает массив из 8 элементов и выводит их на экран");
+                                            Console.WriteLine();
+Console.Write("Введите минимальное значение в массиве:");
+int minValue = EnterParam ();
+                                            Console.WriteLine();
+Console.Write("Введите максимальное значение в массиве:");
+int maxValue = EnterParam ();
+                                            Console.WriteLine();
+Console.Write($"{String.Join(", ", Fillarr(minValue, maxValue))}");
+                                            Console.WriteLine();
+
+// Метод - Ввод параметров
+int EnterParam ()
+{
+int val = Convert.ToInt32(Console.ReadLine()); return val;
+}
+// метод - Заполнение массива
+int[] Fillarr (int min, int max)
+{
+    int[] arr = new int[8];
+        
+    for (int i = 0; i < 8; i++) arr[i] = new Random().Next(min,max+1);
+    
+    return arr;
+}
